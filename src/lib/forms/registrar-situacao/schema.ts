abd99c9ee8c7593e366210132e1d10245d791327
Luks17 <lucasthidavort@gmail.com>
2024-04-18 21:50:43 -0400

    Forms for new entities

--- src/lib/forms/registrar-situacao/schema.ts
+++ src/lib/forms/registrar-situacao/schema.ts
@@ -4,7 +4,16 @@ import { enumValues } from "../common";
 
 export const registrarSituacaoSchema = yup.object({
   moradia: yup.mixed<MoradiaEnum>().oneOf(enumValues(MoradiaEnum)).required(),
+  valor_aluguel: yup
+    .number()
+    .positive()
+    .when("moradia", {
+      is: (val: MoradiaEnum) => val === MoradiaEnum.Aluguel,
+      then: (schema) => schema.required(),
+    }),
   total_adultos: yup.number().positive().integer().required(),
+  total_criancas: yup.number().positive().integer().required(),
+  renda_familiar: yup.number().positive().required(),
 });
 
 export type RegistrarSituacaoFormData = yup.InferType<
--- src/lib/forms/solicitacao-auxilio/schema.ts
+++ src/lib/forms/solicitacao-auxilio/schema.ts
@@ -1,4 +1,4 @@
-import { TiposProblemas } from "@/database/models/Solicitacao";
+import { TiposAuxilios, TiposProblemas } from "@/database/models/Solicitacao";
 import * as yup from "yup";
 import { enumValues } from "../common";
 
@@ -7,6 +7,42 @@ export const solicitacaoAuxilioSchema = yup.object({
     .mixed<TiposProblemas>()
     .oneOf(enumValues(TiposProblemas))
     .required(),
+  descricao_problema: yup.string().min(20).required(),
+  tipo_auxilio: yup
+    .mixed<TiposAuxilios>()
+    .oneOf(enumValues(TiposAuxilios))
+    .required(),
+  qtd_cestas_basica: yup
+    .number()
+    .positive()
+    .integer()
+    .when("tipo_auxilio", {
+      is: (val: TiposAuxilios) => val === TiposAuxilios["Cesta Básica"],
+      then: (schema) => schema.required(),
+    }),
+  vl_auxilio_medicamento: yup
+    .number()
+    .positive()
+    .when("tipo_auxilio", {
+      is: (val: TiposAuxilios) => val === TiposAuxilios["Auxílio Medicamento"],
+      then: (schema) => schema.required(),
+    }),
+  qtd_vagas_creche: yup
+    .number()
+    .positive()
+    .integer()
+    .when("tipo_auxilio", {
+      is: (val: TiposAuxilios) => val === TiposAuxilios["Vaga para Creche"],
+      then: (schema) => schema.required(),
+    }),
+  qtd_vagas_escola: yup
+    .number()
+    .positive()
+    .integer()
+    .when("tipo_auxilio", {
+      is: (val: TiposAuxilios) => val === TiposAuxilios["Vaga para Escola"],
+      then: (schema) => schema.required(),
+    }),
 });
 
 export type SolicitacaoAuxilioFormData = yup.InferType<
