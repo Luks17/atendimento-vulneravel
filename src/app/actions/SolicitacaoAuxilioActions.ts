b4b0c92ac4ae5467c9f59e263459975699d40aa5
Luks17 <lucasthidavort@gmail.com>
2024-08-04 19:15:04 -0400

    Fix solicitacao not updating but creating a new one

--- src/app/actions/SolicitacaoAuxilioActions.ts
+++ src/app/actions/SolicitacaoAuxilioActions.ts
@@ -2,6 +2,7 @@
 
 import { SolicitacaoService } from "@/database/services/SolicitacaoService";
 import { CreateSolicitacaoDTO } from "@/lib/DTO/Solicitacao/CreateSolicitacaoDTO";
+import { UpdateSolicitacaoDTO } from "@/lib/DTO/Solicitacao/UpdateSolicitacaoDTO";
 import Response, { SimpleReturn } from "@/lib/Response";
 import { validateRequest } from "@/lib/auth/Session";
 import { ServerError } from "@/lib/error/ServerError";
@@ -9,66 +10,92 @@ import { mapAndTraceError } from "@/lib/error/util";
 import { SolicitacaoAuxilioFormData } from "@/lib/ui/forms/solicitacao-auxilio/schema";
 
 export async function list() {
-  try {
-    const { session, user } = await validateRequest();
+	try {
+		const { session, user } = await validateRequest();
 
-    if (!session) {
-      throw new ServerError(
-        "NO_AUTH",
-        "Unauthorized to list solicitacoes auxilio",
-      );
-    }
+		if (!session) {
+			throw new ServerError(
+				"NO_AUTH",
+				"Unauthorized to list solicitacoes auxilio",
+			);
+		}
 
-    return await SolicitacaoService.findAll({ usuario_id: user.id });
-  } catch (e) {
-    return mapAndTraceError(e);
-  }
+		return await SolicitacaoService.findAll({ usuario_id: user.id });
+	} catch (e) {
+		return mapAndTraceError(e);
+	}
 }
 
 export async function submitSolicitacaoAuxilio(
-  data: SolicitacaoAuxilioFormData,
+	data: SolicitacaoAuxilioFormData,
 ): Promise<Response<SimpleReturn>> {
-  try {
-    const { session } = await validateRequest();
+	try {
+		const { session } = await validateRequest();
 
-    if (!session) {
-      throw new ServerError("NO_AUTH", "Unauthorized to create solicitacao");
-    }
+		if (!session) {
+			throw new ServerError("NO_AUTH", "Unauthorized to create solicitacao");
+		}
 
-    const dto = await CreateSolicitacaoDTO.fromFormData(data);
+		const dto = await CreateSolicitacaoDTO.fromFormData(data);
 
-    await SolicitacaoService.new(dto);
+		await SolicitacaoService.new(dto);
 
-    return {
-      success: true,
-      data: {
-        message: "Solicitacao Criada com Sucesso",
-      },
-    };
-  } catch (e) {
-    return mapAndTraceError(e);
-  }
+		return {
+			success: true,
+			data: {
+				message: "Solicitacao Criada com Sucesso",
+			},
+		};
+	} catch (e) {
+		return mapAndTraceError(e);
+	}
+}
+
+export async function updateSolicitacaoAuxilio(
+	id: string,
+	data: SolicitacaoAuxilioFormData,
+): Promise<Response<SimpleReturn>> {
+	try {
+		const { session } = await validateRequest();
+
+		if (!session) {
+			throw new ServerError("NO_AUTH", "Unauthorized to update solicitacao");
+		}
+
+		const dto = await UpdateSolicitacaoDTO.fromFormData(data);
+
+		await SolicitacaoService.update({ id }, dto);
+
+		return {
+			success: true,
+			data: {
+				message: "Solicitacao Atualizada com Sucesso",
+			},
+		};
+	} catch (e) {
+		return mapAndTraceError(e);
+	}
 }
 
 export async function getSolicitacaoAuxilio(id: string) {
-  try {
-    const { session } = await validateRequest();
+	try {
+		const { session } = await validateRequest();
 
-    if (!session) {
-      throw new ServerError("NO_AUTH", "Unauthorized to get solicitacao");
-    }
+		if (!session) {
+			throw new ServerError("NO_AUTH", "Unauthorized to get solicitacao");
+		}
 
-    const solicitacao = await SolicitacaoService.findOne({ id });
+		const solicitacao = await SolicitacaoService.findOne({ id });
 
-    if (!solicitacao) {
-      throw new ServerError("NOT_FOUND", "Solicitacao not found");
-    }
+		if (!solicitacao) {
+			throw new ServerError("NOT_FOUND", "Solicitacao not found");
+		}
 
-    return {
-      success: true,
-      data: solicitacao,
-    };
-  } catch (e) {
-    return mapAndTraceError(e);
-  }
+		return {
+			success: true,
+			data: solicitacao,
+		};
+	} catch (e) {
+		return mapAndTraceError(e);
+	}
 }
--- src/app/dashboard/solicitacao-auxilio/SolicitacaoForm.tsx
+++ src/app/dashboard/solicitacao-auxilio/SolicitacaoForm.tsx
@@ -5,17 +5,16 @@ import FormCarousel from "@/app/(components)/dashboard/FormCarousel";
 import ComboBox from "@/app/(components)/formWidgets/uncontrolled/ComboBox";
 import Input from "@/app/(components)/formWidgets/uncontrolled/Input";
 import { useSetNotification } from "@/app/(components)/notifications/NotificationProvider";
-import { submitSolicitacaoAuxilio } from "@/app/actions/SolicitacaoAuxilioActions";
+import {
+	submitSolicitacaoAuxilio,
+	updateSolicitacaoAuxilio,
+} from "@/app/actions/SolicitacaoAuxilioActions";
 import { Solicitacao } from "@/database/models/Solicitacao";
-import { AuxilioMedicamento } from "@/database/models/Solicitacao/AuxilioMedicamento";
-import { CestaBasica } from "@/database/models/Solicitacao/CestaBasica";
-import { VagaCreche } from "@/database/models/Solicitacao/VagaCreche";
-import { VagaEscola } from "@/database/models/Solicitacao/VagaEscola";
 import { TiposAuxilios, TiposProblemas } from "@/lib/enums/Solicitacao";
 import { enumEntries } from "@/lib/enums/common";
 import {
-  SolicitacaoAuxilioFormData,
-  solicitacaoAuxilioSchema,
+	SolicitacaoAuxilioFormData,
+	solicitacaoAuxilioSchema,
 } from "@/lib/ui/forms/solicitacao-auxilio/schema";
 import { sections } from "@/lib/ui/forms/solicitacao-auxilio/sections";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -23,169 +22,168 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface Props {
-  solicitacaoJSON?: string;
+	solicitacaoJSON?: string;
 }
 
 function SolicitacaoForm({ solicitacaoJSON }: Props) {
-  const [currentSection, setCurrentSection] = useState(0);
-  const setNotification = useSetNotification();
+	const [currentSection, setCurrentSection] = useState(0);
+	const setNotification = useSetNotification();
 
-  const solicitacao:
-    | Solicitacao
-    | AuxilioMedicamento
-    | CestaBasica
-    | VagaCreche
-    | VagaEscola
-    | null = solicitacaoJSON ? JSON.parse(solicitacaoJSON) : null;
+	const solicitacao: Solicitacao | null = solicitacaoJSON
+		? JSON.parse(solicitacaoJSON)
+		: null;
 
-  const {
-    register,
-    reset,
-    handleSubmit,
-    trigger,
-    watch,
-    formState: { errors },
-  } = useForm<SolicitacaoAuxilioFormData>({
-    resolver: yupResolver(solicitacaoAuxilioSchema),
-  });
+	const {
+		register,
+		reset,
+		handleSubmit,
+		trigger,
+		watch,
+		formState: { errors },
+	} = useForm<SolicitacaoAuxilioFormData>({
+		resolver: yupResolver(solicitacaoAuxilioSchema),
+	});
 
-  const tipoAuxilio = watch(
-    "tipo_auxilio",
-    TiposAuxilios["Auxílio Medicamento"],
-  );
+	const tipoAuxilio = watch(
+		"tipo_auxilio",
+		TiposAuxilios["Auxílio Medicamento"],
+	);
 
-  useEffect(() => {
-    if (solicitacao) {
-      reset({
-        ...solicitacao,
-        tipo_auxilio: solicitacao.descriminador,
-        qtd_cestas_basica: solicitacao.quantidade_cestas,
-      });
-    }
-  }, []);
+	useEffect(() => {
+		if (solicitacao) {
+			reset({
+				...solicitacao,
+				tipo_auxilio: solicitacao.descriminador,
+				qtd_cestas_basica: solicitacao.quantidade_cestas,
+			});
+		}
+	}, []);
 
-  function changeSection(
-    e: React.MouseEvent<HTMLButtonElement>,
-    next: boolean,
-  ) {
-    e.preventDefault();
+	function changeSection(
+		e: React.MouseEvent<HTMLButtonElement>,
+		next: boolean,
+	) {
+		e.preventDefault();
 
-    const currentSectionItems = sections[currentSection].items;
+		const currentSectionItems = sections[currentSection].items;
 
-    trigger(next ? currentSectionItems : []).then((isValid) => {
-      if (isValid) {
-        const newCurrentSection = next
-          ? currentSection + 1
-          : currentSection - 1;
+		trigger(next ? currentSectionItems : []).then((isValid) => {
+			if (isValid) {
+				const newCurrentSection = next
+					? currentSection + 1
+					: currentSection - 1;
 
-        document
-          .querySelector(`#form-carousel-section-${newCurrentSection}`)
-          ?.scrollIntoView();
+				document
+					.querySelector(`#form-carousel-section-${newCurrentSection}`)
+					?.scrollIntoView();
 
-        setCurrentSection(newCurrentSection);
-      }
-    });
-  }
+				setCurrentSection(newCurrentSection);
+			}
+		});
+	}
 
-  return (
-    <Form.SectionsWrapper currentSection={currentSection} sections={sections}>
-      <Form.Root
-        onSubmit={handleSubmit(async (formData) => {
-          const { success, data } = await submitSolicitacaoAuxilio(formData);
+	return (
+		<Form.SectionsWrapper currentSection={currentSection} sections={sections}>
+			<Form.Root
+				onSubmit={handleSubmit(async (formData) => {
+					const { success, data } =
+						solicitacao === null
+							? await submitSolicitacaoAuxilio(formData)
+							: await updateSolicitacaoAuxilio(solicitacao.id, formData);
 
-          if (success) {
-            setNotification({ message: data.message, messageType: "success" });
-          } else {
-            setNotification({ message: data.message, messageType: "error" });
-          }
-        })}
-      >
-        <FormCarousel.Root>
-          <FormCarousel.Section id={0}>
-            <ComboBox
-              register={register("tipo_problema")}
-              enumOptions={enumEntries(TiposProblemas)}
-              label="Tipo de Problema"
-              error={errors.tipo_problema}
-            />
-            <Input
-              register={register("descricao_problema")}
-              label="Descrição do Problema"
-              error={errors.descricao_problema}
-            />
-          </FormCarousel.Section>
-          <FormCarousel.Section id={1}>
-            <ComboBox
-              register={register("tipo_auxilio")}
-              enumOptions={enumEntries(TiposAuxilios)}
-              label="Tipo de Auxílio"
-              error={errors.tipo_auxilio}
-            />
-            {tipoAuxilio === TiposAuxilios["Cesta Básica"] && (
-              <div className="animate-[fade-in_.5s]">
-                <Input
-                  register={register("qtd_cestas_basica", {
-                    shouldUnregister: true,
-                  })}
-                  label="Quantidade de Cestas Básicas"
-                  type="number"
-                  defaultValue={0}
-                  size={5}
-                  error={errors.qtd_cestas_basica}
-                />
-              </div>
-            )}
-            {tipoAuxilio === TiposAuxilios["Auxílio Medicamento"] && (
-              <div className="animate-[fade-in_.5s]">
-                <Input
-                  register={register("vl_auxilio_medicamento", {
-                    shouldUnregister: true,
-                  })}
-                  label="Valor de Auxílio Medicamento"
-                  size={10}
-                  placeholder="R$"
-                  error={errors.vl_auxilio_medicamento}
-                />
-              </div>
-            )}
-            {tipoAuxilio === TiposAuxilios["Vaga para Creche"] && (
-              <div className="animate-[fade-in_.5s]">
-                <Input
-                  register={register("qtd_vagas_creche", {
-                    shouldUnregister: true,
-                  })}
-                  label="Quantidade de Vagas para Creche"
-                  type="number"
-                  defaultValue={0}
-                  size={5}
-                  error={errors.qtd_vagas_creche}
-                />
-              </div>
-            )}
-            {tipoAuxilio === TiposAuxilios["Vaga para Escola"] && (
-              <div className="animate-[fade-in_.5s]">
-                <Input
-                  register={register("qtd_vagas_escola", {
-                    shouldUnregister: true,
-                  })}
-                  label="Quantidade de Vagas para Escola"
-                  type="number"
-                  defaultValue={0}
-                  size={5}
-                  error={errors.qtd_vagas_escola}
-                />
-              </div>
-            )}
-          </FormCarousel.Section>
-        </FormCarousel.Root>
-        <Form.SubmitWithSections
-          currentSection={currentSection}
-          sectionsNumber={sections.length}
-          changeSection={changeSection}
-        />
-      </Form.Root>
-    </Form.SectionsWrapper>
-  );
+					if (success) {
+						setNotification({ message: data.message, messageType: "success" });
+					} else {
+						setNotification({ message: data.message, messageType: "error" });
+					}
+				})}
+			>
+				<FormCarousel.Root>
+					<FormCarousel.Section id={0}>
+						<ComboBox
+							register={register("tipo_problema")}
+							enumOptions={enumEntries(TiposProblemas)}
+							label="Tipo de Problema"
+							error={errors.tipo_problema}
+						/>
+						<Input
+							register={register("descricao_problema")}
+							label="Descrição do Problema"
+							error={errors.descricao_problema}
+						/>
+					</FormCarousel.Section>
+					<FormCarousel.Section id={1}>
+						<ComboBox
+							register={register("tipo_auxilio")}
+							enumOptions={enumEntries(TiposAuxilios)}
+							label="Tipo de Auxílio"
+							error={errors.tipo_auxilio}
+						/>
+						{tipoAuxilio === TiposAuxilios["Cesta Básica"] && (
+							<div className="animate-[fade-in_.5s]">
+								<Input
+									register={register("qtd_cestas_basica", {
+										shouldUnregister: true,
+									})}
+									label="Quantidade de Cestas Básicas"
+									type="number"
+									defaultValue={0}
+									size={5}
+									error={errors.qtd_cestas_basica}
+								/>
+							</div>
+						)}
+						{tipoAuxilio === TiposAuxilios["Auxílio Medicamento"] && (
+							<div className="animate-[fade-in_.5s]">
+								<Input
+									register={register("vl_auxilio_medicamento", {
+										shouldUnregister: true,
+									})}
+									label="Valor de Auxílio Medicamento"
+									size={10}
+									placeholder="R$"
+									error={errors.vl_auxilio_medicamento}
+								/>
+							</div>
+						)}
+						{tipoAuxilio === TiposAuxilios["Vaga para Creche"] && (
+							<div className="animate-[fade-in_.5s]">
+								<Input
+									register={register("qtd_vagas_creche", {
+										shouldUnregister: true,
+									})}
+									label="Quantidade de Vagas para Creche"
+									type="number"
+									defaultValue={0}
+									size={5}
+									error={errors.qtd_vagas_creche}
+								/>
+							</div>
+						)}
+						{tipoAuxilio === TiposAuxilios["Vaga para Escola"] && (
+							<div className="animate-[fade-in_.5s]">
+								<Input
+									register={register("qtd_vagas_escola", {
+										shouldUnregister: true,
+									})}
+									label="Quantidade de Vagas para Escola"
+									type="number"
+									defaultValue={0}
+									size={5}
+									error={errors.qtd_vagas_escola}
+								/>
+							</div>
+						)}
+					</FormCarousel.Section>
+				</FormCarousel.Root>
+				<Form.SubmitWithSections
+					currentSection={currentSection}
+					sectionsNumber={sections.length}
+					changeSection={changeSection}
+				/>
+			</Form.Root>
+		</Form.SectionsWrapper>
+	);
 }
 
 export default SolicitacaoForm;
